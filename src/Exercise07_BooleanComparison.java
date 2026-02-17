1fe566d4c225d436b14f0a193c8df4ee6cec968f
Nicolle de Assis <nicolledeassis13@gmail.com>
2026-02-17 20:48:30 -0300

    feature: adicionado exercicio 07

---
+++ src/Exercise07_BooleanComparison.java
@@ -0,0 +1,13 @@
+import java.io.FilterOutputStream;
+
+public class Exercise07_BooleanComparison {
+    public static void main(String[] args) {
+
+        boolean valueA = false;
+        boolean valueB = true;
+
+        boolean result = valueA && valueB;
+
+        System.out.println(result ? "VERDADEIRO" : "FALSO");
+    }
+}
