33f58b67099ab1e3f23ba4e883cd4292c811c7cd
Nicolle de Assis <nicolledeassis13@gmail.com>
2026-02-17 19:52:54 -0300

    feature: adicionando exercicio 04

---
+++ src/ExerciciosDeLogica/Exercise04_PredecessorSuccessor.java
@@ -0,0 +1,14 @@
+package ExerciciosDeLogica;
+
+public class Exercise04_PredecessorSuccessor {
+    public static void main(String[] args) {
+
+        int value = 4;
+        int predecessor = value - 1;
+        int successor = value + 1;
+
+        System.out.println("Valor: "+value);
+        System.out.println("Antecessor: "+predecessor);
+        System.out.println("Sucessor: "+successor);
+    }
+}
