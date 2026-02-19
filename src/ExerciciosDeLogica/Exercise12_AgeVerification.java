783968470c21d9ce4838aad7524cfb8016c06b56
Nicolle de Assis <nicolledeassis13@gmail.com>
2026-02-19 20:16:25 -0300

    feature: adicionado exercicio 12

---
+++ src/ExerciciosDeLogica/Exercise12_AgeVerification.java
@@ -0,0 +1,16 @@
+package ExerciciosDeLogica;
+
+public class Exercise12_AgeVerification {
+    public static void main(String[] args) {
+
+        String name = "Nicolle";
+        int age = 20;
+
+        if (age>=18){
+            System.out.println("A usuária "+name+" já é maior de idade");
+        }else{
+            System.out.println("A usuária "+name+" não é maior de idade");
+        }
+
+    }
+}
